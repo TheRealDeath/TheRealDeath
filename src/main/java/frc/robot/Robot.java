a4224f7592c67728cd5da5012150a8700bf75ebb
Gerardo Vega <you@example.com>
2022-03-10 19:02:50 -0600

    edited and auton

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,22 +1,23 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+
 
 package frc.robot;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import javax.lang.model.util.ElementScanner6;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 
-
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends TimedRobot {
   
   private DifferentialDrive m_myRobot;
@@ -34,23 +35,11 @@ public class Robot extends TimedRobot {
   private CANSparkMax m_armMotor1;
   private CANSparkMax m_intakeMotor1;
   private int previousPOV;
-  private double previousThrottle;
-
+  private double previousThrottle, idleThrottle;
+  private DifferentialDrivetrainSim m_driveSim;
+  private MotorControllerGroup m_left,m_right;
   @Override
   public void robotInit() {
-  /**
-   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
-   * 
-   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
-   * first parameter
-   * 
-   * The motor type is passed as the second parameter. Motor type can either be:
-   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
-   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
-   * 
-   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
-   * these parameters to match your setup
-   */
     m_leftMotor1 = new CANSparkMax(leftDeviceID1, MotorType.kBrushless);
     m_rightMotor1 = new CANSparkMax(rightDeviceID1, MotorType.kBrushless);
     m_leftMotor2 = new CANSparkMax(leftDeviceID2, MotorType.kBrushless);
@@ -58,45 +47,87 @@ public class Robot extends TimedRobot {
     m_armMotor1 = new CANSparkMax(armDeviceID1,MotorType.kBrushless);
     m_intakeMotor1 = new CANSparkMax(intakeDeviceId1,MotorType.kBrushless);
     m_intakeMotor1.setSmartCurrentLimit(30);
-    MotorControllerGroup m_left = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
-    MotorControllerGroup m_right = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
-
+    m_left = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
+    m_right = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
     m_leftMotor1.restoreFactoryDefaults();
     m_rightMotor1.restoreFactoryDefaults();
     m_leftMotor2.restoreFactoryDefaults();
     m_rightMotor2.restoreFactoryDefaults();
     m_armMotor1.restoreFactoryDefaults();
+    m_armMotor1.enableSoftLimit(SoftLimitDirection.kForward, true);
+    m_armMotor1.setSoftLimit(SoftLimitDirection.kForward, 0);
+    m_armMotor1.enableSoftLimit(SoftLimitDirection.kReverse, true);
+    m_armMotor1.setSoftLimit(SoftLimitDirection.kReverse, (float)-12.67);
     m_intakeMotor1.restoreFactoryDefaults();
 
     m_myRobot = new DifferentialDrive(m_left, m_right);
 
     leftStick = new Joystick(0);
-    leftStick.toString();
+    idleThrottle = leftStick.getRawAxis(1);
   }
-
+  public void simulationPeriodic() {
+    // Set the inputs to the system. Note that we need to convert
+    // the [-1, 1] PWM signal to voltage by multiplying it by the
+    // robot controller voltage.
+    m_driveSim.setInputs(m_left.get() * RobotController.getInputVoltage(),m_right.get() * RobotController.getInputVoltage());
+    // Advance the model by 20 ms. Note that if you are running this
+    // subsystem in a separate thread or have changed the nominal timestep
+    // of TimedRobot, this value needs to match it.
+    m_driveSim.update(0.02);
+  }  
   @Override
   public void teleopPeriodic() {
+
     //m_myRobot.tankDrive(-leftStick.getRawAxis(1), leftStick.getRawAxis(5));
-    m_myRobot.arcadeDrive(leftStick.getRawAxis(1), leftStick.getRawAxis(5));
-    int i = 0;
+    m_myRobot.arcadeDrive(-leftStick.getRawAxis(1), leftStick.getRawAxis(5));
     if(leftStick.getPOV() != previousPOV)
     {
-      System.out.println(Math.abs(leftStick.getPOV()-previousPOV));
+      System.out.println(Math.abs(leftStick.getPOV()-previousPOV)-1);
     }
+    
+    //intake
     if(leftStick.getRawButton(3))
+      m_intakeMotor1.set(1);
+    else if(leftStick.getRawButton(4))
+      m_intakeMotor1.set(-1);
+    else
     {
-      while(true)
-      {
-        System.out.println("going up");
-        if(i++ == 20)
-          break;
-      }
+      System.out.println(m_intakeMotor1.get());
+      m_intakeMotor1.set(0);
     }
-    if(leftStick.getRawAxis(1) != previousThrottle)
+
+    if(leftStick.getRawAxis(1) != previousThrottle && idleThrottle != leftStick.getRawAxis(1))
     {
       System.out.println(leftStick.getRawAxis(1));
     }
     previousThrottle = leftStick.getRawAxis(1);
     previousPOV = leftStick.getPOV();
-  }
+
+    //arm
+    if(leftStick.getRawButton(1))
+      m_armMotor1.set(1);
+    else if(leftStick.getRawButton(2))
+      m_armMotor1.set(-1);  
+    else
+      m_armMotor1.set(0);
+    
+    
+    }
+
+    public void autonomousPeriod() {
+      
+      m_intakeMotor1.set(-1);
+
+      if(Timer.getMatchTime() < 14)  {
+        m_intakeMotor1.set(0);
+        while(Timer.getMatchTime() > 9) {
+          m_myRobot.arcadeDrive(-0.4, 0);
+        }
+
+      }
+      
+           
+      
+    }
+      
 }
